0c07be23b2803e9f55df83456d2a09666eef7fdf
ajsimbra <ajsimbra.012@gmail.com>
2023-12-01 01:03:53 +0800

    Create Profile and Initial Design in profile.css

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,98 +1,83 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { Button, Img, Line, Text } from "components";
+import { Button } from "components";
+import Header from "components/Header";
 import "./Profile.css";
 
 const Profile = () => {
-  const navigate = useNavigate();
+  // Initial state with dummy user data
+  const [userData, setUserData] = useState({
+    firstName: 'John',
+    lastName: 'Doe',
+    email: 'john.doe@example.com',
+    password: 'securepassword',
+  });
 
-  // Step 1: Add state variables
-  const [firstName, setFirstName] = useState("John");
-  const [lastName, setLastName] = useState("Doe");
-  const [email, setEmail] = useState("john.doe@example.com");
-  const [password, setPassword] = useState("********");
+  // State to manage form inputs
+  const [formData, setFormData] = useState({});
 
-  // Step 2: Create update functions
-  const updateFirstName = () => {
-    const newFirstName = prompt("Enter new First Name:", firstName);
-    if (newFirstName !== null) {
-      setFirstName(newFirstName);
-    }
+  // Function to handle input changes
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
   };
 
-  const updateLastName = () => {
-    const newLastName = prompt("Enter new Last Name:", lastName);
-    if (newLastName !== null) {
-      setLastName(newLastName);
-    }
-  };
-
-  const updateEmail = () => {
-    const newEmail = prompt("Enter new Email:", email);
-    if (newEmail !== null) {
-      setEmail(newEmail);
-    }
-  };
-
-  const updatePassword = () => {
-    const newPassword = prompt("Enter new Password:", password);
-    if (newPassword !== null) {
-      setPassword(newPassword);
-    }
+  // Function to handle form submission
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Update user data with new form data
+    setUserData({ ...userData, ...formData });
+    // Reset form data
+    setFormData({});
   };
 
   return (
     <>
-      {/* ... existing code ... */}
-
-      <div className="flex flex-col gap-4 items-center justify-start md:px-5 w-[31%] md:w-full">
-        {/* Display current user information */}
-        <div>
-          <p>First Name: {firstName}</p>
-          <p>Last Name: {lastName}</p>
-          <p>Email: {email}</p>
-          <p>Password: {password}</p>
-        </div>
-
-        {/* Button to update First Name */}
-        <Button
-          onClick={updateFirstName}
-          color="blue_gray_800"
-          size="xs"
-          variant="fill"
-        >
-          Update First Name
-        </Button>
-
-        {/* Button to update Last Name */}
-        <Button
-          onClick={updateLastName}
-          color="blue_gray_800"
-          size="xs"
-          variant="fill"
-        >
-          Update Last Name
-        </Button>
-
-        {/* Button to update Email */}
-        <Button
-          onClick={updateEmail}
-          color="blue_gray_800"
-          size="xs"
-          variant="fill"
-        >
-          Update Email
-        </Button>
-
-        {/* Button to update Password */}
-        <Button
-          onClick={updatePassword}
-          color="blue_gray_800"
-          size="xs"
-          variant="fill"
-        >
-          Update Password
-        </Button>
+      <Header />
+      <div className="profile-container">
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="firstName">First Name:</label>
+            <input
+              type="text"
+              id="firstName"
+              name="firstName"
+              defaultValue={userData.firstName}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="lastName">Last Name:</label>
+            <input
+              type="text"
+              id="lastName"
+              name="lastName"
+              defaultValue={userData.lastName}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="email">Email:</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              defaultValue={userData.email}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="password">Password:</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              defaultValue={userData.password}
+              onChange={handleInputChange}
+            />
+          </div>
+          <button type="submit">Save Changes</button>
+        </form>
       </div>
     </>
   );
