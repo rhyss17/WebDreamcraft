7199ec12d05a33e8ca925b66a4add2f60d5946ef
Rhyss <rhyssgianne123@gmail.com>
2023-12-05 20:26:01 +0800

    putting hover in header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,6 +3,7 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 
 import { Button, Img, Text } from "components";
+import "./Header.css";
 
 const Header = (props) => {
   const navigate = useNavigate();
